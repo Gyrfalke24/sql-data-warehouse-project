1d9bd45760f9238c9b7a8cea21198be7bfdb548b
Gyrfalke24 <dnageorge@gmail.com>
2025-05-15 14:09:44 -0600

    modify absolute paths to relative paths

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -34,7 +34,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info ;
 		PRINT '>> Inserting Date Into: bronze.crm_cust_info'
 		BULK INSERT bronze.crm_cust_info 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM '..\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -49,7 +49,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> Inserting Date Into: bronze.crm_prd_info'
 		BULK INSERT bronze.crm_prd_info 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM '..\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -64,7 +64,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Date Into: bronze.crm_sales_details'
 		BULK INSERT bronze.crm_sales_details 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM '..\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -83,7 +83,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Date Into: bronze.erp_cust_az12'
 		BULK INSERT bronze.erp_cust_az12 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM '..\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -98,7 +98,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Date Into: bronze.erp_loc_a101'
 		BULK INSERT bronze.erp_loc_a101 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM '..\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -113,7 +113,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Date Into: bronze.erp_px_cat_g1v2'
 		BULK INSERT bronze.erp_px_cat_g1v2 
-		FROM 'D:\George\Documents\Courses\Modern_Data_Warehouse\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM '..\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
